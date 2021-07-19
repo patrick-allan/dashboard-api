a2b4fae0928b83324a3393ca9e24f1e66213024e
patrick-allan <prkallan1337@gmail.com>
2021-07-19 10:20:58 -0300

    modificações banco de dados e README.md

--- README.md
+++ README.md
@@ -1,12 +1,14 @@
-# Dashboard - API #
+# Dashboard - API
+Essa aplicação é o backend do Dashboard (https://github.com/patrick-allan/dashboard).
 
-# Pacotes
-go get github.com/gorilla/mux
-go get github.com/mattn/go-sqlite3
-go get github.com/joho/godotenv
-go get github.com/dgrijalva/jwt-go
-go get github.com/fatih/color
-go get golang.org/x/crypto/bcrypt
+## Requisitos Windows
+ - TDM GCC (https://jmeubank.github.io/tdm-gcc/)
 
-# Build and Run
+## Requisitos Linux
+ - GCC 
+
+### Build 
+go build C:/Go/dashboard-api/cmd/dashboard.go
+
+### Build and Run
 go build C:/Go/dashboard-api/cmd/dashboard.go && .\dashboard.exe
--- pkg/dbms/model/users.go
+++ pkg/dbms/model/users.go
@@ -3,11 +3,35 @@ package model
 import "time"
 
 type User struct {
+	Id          int           `json:"id,omitempty"`
+	Name        string        `json:"name,omitempty"`
+	Email       string        `json:"email,omitempty"`
+	Username    string        `json:"username,omitempty"`
+	Password    string        `json:"password,omitempty"`
+	Birthdate   string        `json:"birthdate,omitempty"`
+	Document    string        `json:"document,omitempty"`
+	Status      string        `json:"status,omitempty"`
+	Created_at  *time.Time    `json:"created_at,omitempty"`
+	Updated_at  *time.Time    `json:"updated_at,omitempty"`
+	Deleted_at  *time.Time    `json:"deleted_at,omitempty"`
+	Gender      []Gender      `json:"gender,omitempty"`
+	Nationality []Nationality `json:"nationality,omitempty"`
+}
+
+type Gender struct {
 	Id         int        `json:"id,omitempty"`
-	Name       string     `json:"name,omitempty"`
-	Email      string     `json:"email,omitempty"`
-	Password   string     `json:"password,omitempty"`
+	Type       string     `json:"type,omitempty"`
 	Status     string     `json:"status,omitempty"`
 	Created_at *time.Time `json:"created_at,omitempty"`
 	Updated_at *time.Time `json:"updated_at,omitempty"`
+	Deleted_at *time.Time `json:"deleted_at,omitempty"`
+}
+
+type Nationality struct {
+	Id          int        `json:"id,omitempty"`
+	Nationality string     `json:"nationality,omitempty"`
+	Status      string     `json:"status,omitempty"`
+	Created_at  *time.Time `json:"created_at,omitempty"`
+	Updated_at  *time.Time `json:"updated_at,omitempty"`
+	Deleted_at  *time.Time `json:"deleted_at,omitempty"`
 }
